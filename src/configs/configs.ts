b9c3493b0f1234366ba4a3a2b7f1a2698711cf10
Panco <panco@pancic.com>
2022-11-18 22:36:59 +0100

    Update environment variables format and app configurations.

--- src/configs/configs.ts
+++ src/configs/configs.ts
@@ -1,36 +1,5 @@
-interface ServerConfig {
-    serverPort: number;
-    serverLocalAddress: string;
-    serverRemoteAddress: string;
-    useServerRemoteAddress: boolean;
-}
+// get server configurations
+const serverPort = Number(process.env.REACT_APP_SERVER_PORT) || 5000;
+const serverAddress = process.env.REACT_APP_SERVER_ADDRESS || "localhost";
 
-// when server is deployed, then it's always use HTTPS 443 predefined port
-// there are cases when HTTP 80 port can be used for secured HTTPS 443
-// transfer when port 443 is not available
-// other ports than HTTPS 443 will not work on production site or app
-const serverConfigurations: ServerConfig = {
-    serverPort: Number(process.env.REACT_APP_SERVER_PORT),
-
-    serverLocalAddress:
-        process.env.REACT_APP_SERVER_LOCAL_ADDRESS || "localhost",
-
-    serverRemoteAddress: process.env.REACT_APP_SERVER_REMOTE_ADDRESS || "",
-
-    // use true - online development (when deployed)
-    // use false - local development (when debugging)
-    useServerRemoteAddress:
-        process.env.REACT_APP_USE_SERVER_REMOTE_ADDRESS === "true",
-};
-
-const serverAddress =
-    serverConfigurations.useServerRemoteAddress === true
-        ? serverConfigurations.serverRemoteAddress
-        : serverConfigurations.serverLocalAddress;
-
-// use https secured protocol for deployed environments,
-// otherwise use simple http protocol for local testing
-const clientProtocol =
-    serverConfigurations.useServerRemoteAddress === true ? "https" : "http";
-
-export const serverOrigin = `${clientProtocol}://${serverAddress}:${serverConfigurations.serverPort}`;
+export const serverOrigin = `${serverAddress}:${serverPort}`;
