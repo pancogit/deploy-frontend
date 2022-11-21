1cd2673b05673731e3382c7b254c237000e2b073
Panco <panco@pancic.com>
2022-11-22 00:23:56 +0100

    Use port only for development mode.

--- src/configs/configs.ts
+++ src/configs/configs.ts
@@ -2,4 +2,7 @@
 const serverPort = Number(process.env.REACT_APP_SERVER_PORT) || 5000;
 const serverAddress = process.env.REACT_APP_SERVER_ADDRESS || "localhost";
 
-export const serverOrigin = `${serverAddress}:${serverPort}`;
+export const serverOrigin =
+    process.env.NODE_ENV === "development"
+        ? `${serverAddress}:${serverPort}`
+        : serverAddress;
